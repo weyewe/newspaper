69f4a85c458ec1cff328d825ee986c1461b28511
weyewe <w.yunnal@gmail.com>
2011-09-25 20:24:26 +0700

    last

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Newspaper::Application.initialize!
 
-
-SENDGRID_USERNAME = app1074306@heroku.com
-SENDGRID_PASSWORD
-SENDGRID_DOMAIN
